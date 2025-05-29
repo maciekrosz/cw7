f5f32c9e1721fb632dedfbcc1f737aac6ce5d87b
s33498 <s33498@pjwstk.edu.pl>
2025-05-29 10:53:11 +0200

    cwizenie10 do ogarniecia w domu

--- IHazardNotifier.java
+++ IHazardNotifier.java
@@ -2,8 +2,3 @@ public interface IHazardNotifier {
     void notifyHazard(String message);
 }
 
-public class OverfillException extends Exception {
-    public OverfillException(String message) {
-        super(message);
-    }
-}
